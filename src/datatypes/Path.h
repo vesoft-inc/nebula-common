517ce2fa942fb316450ea2861f7b5e7e273c7213
sherman-the-tank <sherman.ye@vesoft.com>
2020-03-04 20:01:04 +0800

    Improve the graphd thrift interface
    1) ExecuteResponse now support multiple datasets (each dataset is a table with same columns)
    2) Added Vertex and Edge type
    3) Added a new composite type Set
    4) Complete the Path type

--- src/datatypes/Path.h
+++ src/datatypes/Path.h
@@ -9,31 +9,50 @@
 
 #include "base/Base.h"
 #include "thrift/ThriftTypes.h"
+#include "datatypes/Value.h"
+#include "datatypes/Vertex.h"
 
 namespace nebula {
 
 struct Step {
+    Vertex dst;
     EdgeType type;
     EdgeRanking ranking;
-    VertexID dst;
+    std::unordered_map<std::string, Value> props;
+
+    Step() = default;
+    Step(const Step& s) : dst(s.dst)
+                        , type(s.type)
+                        , ranking(s.ranking)
+                        , props(s.props) {}
+    Step(Step&& s) : dst(std::move(s.dst))
+                   , type(std::move(s.type))
+                   , ranking(std::move(s.ranking))
+                   , props(std::move(s.props)) {}
 
     void clear() {
+        dst.clear();
         type = 0;
         ranking = 0;
-        dst.clear();
+        props.clear();
     }
 
     bool operator==(const Step& rhs) const {
-        return type == rhs.type &&
+        return dst == rhs.dst &&
+               type == rhs.type &&
                ranking == rhs.ranking &&
-               dst == rhs.dst;
+               props == rhs.props;
     }
 };
 
 
 struct Path {
-    VertexID src;
+    Vertex src;
     std::vector<Step> steps;
+
+    Path() = default;
+    Path(const Path& p) : src(p.src), steps(p.steps) {}
+    Path(Path&& p) : src(std::move(p.src)), steps(std::move(p.steps)) {}
 
     void clear() {
         src.clear();
@@ -47,5 +66,37 @@ struct Path {
 };
 
 }  // namespace nebula
+
+
+namespace std {
+
+template<>
+struct hash<nebula::Step> {
+    std::size_t operator()(const nebula::Step& h) const noexcept {
+        size_t hv = hash<nebula::Vertex>()(h.dst);
+        hv = folly::hash::fnv64_buf(reinterpret_cast<const void*>(&h.type),
+                                    sizeof(h.type),
+                                    hv);
+        return folly::hash::fnv64_buf(reinterpret_cast<const void*>(&h.ranking),
+                                      sizeof(h.ranking),
+                                      hv);
+    }
+};
+
+
+template<>
+struct hash<nebula::Path> {
+    std::size_t operator()(const nebula::Path& h) const noexcept {
+    size_t hv = hash<nebula::Vertex>()(h.src);
+        for (auto& s : h.steps) {
+            hv += (hv << 1) + (hv << 4) + (hv << 5) + (hv << 7) + (hv << 8) + (hv << 40);
+            hv ^= hash<nebula::Step>()(s);
+        }
+
+        return hv;
+    }
+};
+
+}  // namespace std
 #endif  // DATATYPES_PATH_H_
 
