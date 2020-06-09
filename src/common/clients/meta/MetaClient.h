4e38f0312af4480151f7fdca94360e6de7da800b
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2020-06-09 16:02:05 +0800

    Move auth and permission code to graph. And add some utils to support cloud auth.

--- src/common/clients/meta/MetaClient.h
+++ src/common/clients/meta/MetaClient.h
@@ -495,6 +495,8 @@ public:
 
     bool authCheckFromCache(const std::string& account, const std::string& password);
 
+    bool checkShadowAccountFromCache(const std::string& account);
+
     Status refreshCache();
 
     StatusOr<LeaderMap> loadLeader();
