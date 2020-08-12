680d0b6c0f9fe985d447aa6fc312f7711c089841
yixinglu <2520865+yixinglu@users.noreply.github.com>
2020-08-12 18:59:48 +0800

    Make ExprVisitor pure virtual fn

--- src/common/expression/ExprVisitor.h
+++ src/common/expression/ExprVisitor.h
@@ -40,32 +40,32 @@ class ExprVisitor {
 public:
     virtual ~ExprVisitor() = default;
 
-    virtual void visitConstantExpr(const ConstantExpression *expr);
-    virtual void visitArithmeticExpr(const ArithmeticExpression *expr);
-    virtual void visitUnaryExpr(const UnaryExpression *expr);
-    virtual void visitRelationalExpr(const RelationalExpression *expr);
-    virtual void visitLogicalExpr(const LogicalExpression *expr);
-    virtual void visitTypeCastingExpr(const TypeCastingExpression *expr);
-    virtual void visitFunctionCallExpr(const FunctionCallExpression *expr);
-    virtual void visitEdgePropertyExpr(const EdgePropertyExpression *expr);
-    virtual void visitTagPropertyExpr(const TagPropertyExpression *expr);
-    virtual void visitInputPropertyExpr(const InputPropertyExpression *expr);
-    virtual void visitVariablePropertyExpr(const VariablePropertyExpression *expr);
-    virtual void visitSourcePropertyExpr(const SourcePropertyExpression *expr);
-    virtual void visitDestPropertyExpr(const DestPropertyExpression *expr);
-    virtual void visitEdgeSrcIdExpr(const EdgeSrcIdExpression *expr);
-    virtual void visitEdgeTypeExpr(const EdgeTypeExpression *expr);
-    virtual void visitEdgeRankExpr(const EdgeRankExpression *expr);
-    virtual void visitEdgeDstIdExpr(const EdgeDstIdExpression *expr);
-    virtual void visitUUIDExpr(const UUIDExpression *expr);
-    virtual void visitVariableExpr(const VariableExpression *expr);
-    virtual void visitVersionedVariableExpr(const VersionedVariableExpression *expr);
-    virtual void visitListExpr(const ListExpression *expr);
-    virtual void visitSetExpr(const SetExpression *expr);
-    virtual void visitMapExpr(const MapExpression *expr);
-    virtual void visitLabelExpr(const LabelExpression *expr);
-    virtual void visitSubscriptExpr(const SubscriptExpression *expr);
-    virtual void visitSymbolPropertyExpr(const SymbolPropertyExpression *expr);
+    virtual void visitConstantExpr(const ConstantExpression *expr) = 0;
+    virtual void visitArithmeticExpr(const ArithmeticExpression *expr) = 0;
+    virtual void visitUnaryExpr(const UnaryExpression *expr) = 0;
+    virtual void visitRelationalExpr(const RelationalExpression *expr) = 0;
+    virtual void visitLogicalExpr(const LogicalExpression *expr) = 0;
+    virtual void visitTypeCastingExpr(const TypeCastingExpression *expr) = 0;
+    virtual void visitFunctionCallExpr(const FunctionCallExpression *expr) = 0;
+    virtual void visitEdgePropertyExpr(const EdgePropertyExpression *expr) = 0;
+    virtual void visitTagPropertyExpr(const TagPropertyExpression *expr) = 0;
+    virtual void visitInputPropertyExpr(const InputPropertyExpression *expr) = 0;
+    virtual void visitVariablePropertyExpr(const VariablePropertyExpression *expr) = 0;
+    virtual void visitSourcePropertyExpr(const SourcePropertyExpression *expr) = 0;
+    virtual void visitDestPropertyExpr(const DestPropertyExpression *expr) = 0;
+    virtual void visitEdgeSrcIdExpr(const EdgeSrcIdExpression *expr) = 0;
+    virtual void visitEdgeTypeExpr(const EdgeTypeExpression *expr) = 0;
+    virtual void visitEdgeRankExpr(const EdgeRankExpression *expr) = 0;
+    virtual void visitEdgeDstIdExpr(const EdgeDstIdExpression *expr) = 0;
+    virtual void visitUUIDExpr(const UUIDExpression *expr) = 0;
+    virtual void visitVariableExpr(const VariableExpression *expr) = 0;
+    virtual void visitVersionedVariableExpr(const VersionedVariableExpression *expr) = 0;
+    virtual void visitListExpr(const ListExpression *expr) = 0;
+    virtual void visitSetExpr(const SetExpression *expr) = 0;
+    virtual void visitMapExpr(const MapExpression *expr) = 0;
+    virtual void visitLabelExpr(const LabelExpression *expr) = 0;
+    virtual void visitSubscriptExpr(const SubscriptExpression *expr) = 0;
+    virtual void visitSymbolPropertyExpr(const SymbolPropertyExpression *expr) = 0;
 };
 
 }   // namespace nebula
