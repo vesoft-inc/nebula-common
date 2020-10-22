d0de7c2923cf9e3c5fbd3db41f0c51a56d94b7f1
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2020-10-22 17:16:25 +0800

    Using constant to instead of paramter count number in test.

--- src/common/function/test/FunctionManagerTest.cpp
+++ src/common/function/test/FunctionManagerTest.cpp
@@ -201,27 +201,31 @@ TEST_F(FunctionManagerTest, functionCall) {
         auto res = std::move(result).value()({true});
         EXPECT_EQ(res, Value::kNullBadType);
     }
+    // current time
+    static constexpr std::size_t kCurrentTimeParaNumber = 0;
+    // time from literal
+    static constexpr std::size_t kLiteralTimeParaNumber = 1;
     // date
     {
-        auto result = FunctionManager::get("date", 0);
+        auto result = FunctionManager::get("date", kCurrentTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({});
         EXPECT_EQ(res.type(), Value::Type::DATE);
     }
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({true});
         EXPECT_EQ(res, Value::kNullBadType);
     }
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({"2020-09-15"});
         EXPECT_EQ(res, Value(time::TimeUtils::dateToUTC(Date(2020, 9, 15))));
     }
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 2020}, {"month", 12}, {"day", 31}})});
         EXPECT_EQ(res, Value(time::TimeUtils::dateToUTC(Date(2020, 12, 31))));
@@ -229,34 +233,34 @@ TEST_F(FunctionManagerTest, functionCall) {
     // leap year February days
     {
         // 2020 is leap
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 2020}, {"month", 2}, {"day", 29}})});
         EXPECT_EQ(res, Value(time::TimeUtils::dateToUTC(Date(2020, 2, 29))));
     }
     {
         // 2021 is not leap
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 2021}, {"month", 2}, {"day", 29}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     // month different days
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 2021}, {"month", 1}, {"day", 31}})});
         EXPECT_EQ(res, Value(time::TimeUtils::dateToUTC(Date(2021, 1, 31))));
     }
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 2021}, {"month", 4}, {"day", 31}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     // range [(−32,768, 1, 1), (32,767, 12, 31)]
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", std::numeric_limits<int16_t>::min()},
                                                   {"month", 1}, {"day", 1}})});
@@ -264,7 +268,7 @@ TEST_F(FunctionManagerTest, functionCall) {
                                                              1, 1))));
     }
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", std::numeric_limits<int16_t>::max()},
                                                   {"month", 12}, {"day", 31}})});
@@ -273,141 +277,141 @@ TEST_F(FunctionManagerTest, functionCall) {
     }
     // year
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", -32769}, {"month", 12}, {"day", 15}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32768}, {"month", 12}, {"day", 31}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     // month
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", -32768}, {"month", 13}, {"day", 15}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767}, {"month", 0}, {"day", 31}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     // day
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", -32768}, {"month", 11}, {"day", 0}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("date", 1);
+        auto result = FunctionManager::get("date", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767}, {"month", 1}, {"day", 32}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     // time
     {
-        auto result = FunctionManager::get("time", 0);
+        auto result = FunctionManager::get("time", kCurrentTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({});
         EXPECT_EQ(res.type(), Value::Type::TIME);
     }
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({true});
         EXPECT_EQ(res, Value::kNullBadType);
     }
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({"20:09:15"});
         EXPECT_EQ(res, Value(time::TimeUtils::timeToUTC(Time(20, 9, 15, 0))));
     }
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", 20}, {"minute", 9}, {"second", 15}})});
         EXPECT_EQ(res, Value(time::TimeUtils::timeToUTC(Time(20, 9, 15, 0))));
     }
     // range [(0, 0, 0, 0), (23, 59, 59, 999999)]
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", 0}, {"minute", 0}, {"second", 0}})});
         EXPECT_EQ(res, Value(time::TimeUtils::timeToUTC(Time(0, 0, 0, 0))));
     }
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", 23}, {"minute", 59}, {"second", 59}})});
         EXPECT_EQ(res, Value(time::TimeUtils::timeToUTC(Time(23, 59, 59, 0))));
     }
     // hour
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", -1}, {"minute", 9}, {"second", 15}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", 24}, {"minute", 9}, {"second", 15}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     // minute
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", 23}, {"minute", -1}, {"second", 15}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", 23}, {"minute", 60}, {"second", 15}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     // second
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", 23}, {"minute", 59}, {"second", -1}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("time", 1);
+        auto result = FunctionManager::get("time", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"hour", 23}, {"minute", 59}, {"second", 60}})});
         EXPECT_EQ(res, Value::kNullBadData);
     }
     // datetime
     {
-        auto result = FunctionManager::get("datetime", 0);
+        auto result = FunctionManager::get("datetime", kCurrentTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({});
         EXPECT_EQ(res.type(), Value::Type::DATETIME);
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({true});
         EXPECT_EQ(res, Value::kNullBadType);
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({"2020-09-15T20:09:15"});
         EXPECT_EQ(res, Value(time::TimeUtils::dateTimeToUTC(DateTime(2020, 9, 15, 20, 9, 15, 0))));
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 2020},
                                                    {"month", 9},
@@ -418,7 +422,7 @@ TEST_F(FunctionManagerTest, functionCall) {
         EXPECT_EQ(res, Value(time::TimeUtils::dateTimeToUTC(DateTime(2020, 9, 15, 20, 9, 15, 0))));
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 2020},
                                                    {"month", 9},
@@ -430,7 +434,7 @@ TEST_F(FunctionManagerTest, functionCall) {
     }
     // range [(−32,768, 1, 1, 0, 0, 0, 0), (32,767, 12, 31, 23, 59, 59, 999999)]
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", -32768},
                                                    {"month", 1},
@@ -441,7 +445,7 @@ TEST_F(FunctionManagerTest, functionCall) {
         EXPECT_EQ(res, Value(time::TimeUtils::dateTimeToUTC(DateTime(-32768, 1, 1, 0, 0, 0, 0))));
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 12},
@@ -454,7 +458,7 @@ TEST_F(FunctionManagerTest, functionCall) {
     }
     // year
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 3276700},
                                                    {"month", 12},
@@ -465,7 +469,7 @@ TEST_F(FunctionManagerTest, functionCall) {
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", -3276700},
                                                    {"month", 12},
@@ -477,7 +481,7 @@ TEST_F(FunctionManagerTest, functionCall) {
     }
     // month
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 13},
@@ -488,7 +492,7 @@ TEST_F(FunctionManagerTest, functionCall) {
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 0},
@@ -500,7 +504,7 @@ TEST_F(FunctionManagerTest, functionCall) {
     }
     // day
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 1},
@@ -511,7 +515,7 @@ TEST_F(FunctionManagerTest, functionCall) {
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 1},
@@ -523,7 +527,7 @@ TEST_F(FunctionManagerTest, functionCall) {
     }
     // hour
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 1},
@@ -534,7 +538,7 @@ TEST_F(FunctionManagerTest, functionCall) {
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 1},
@@ -546,7 +550,7 @@ TEST_F(FunctionManagerTest, functionCall) {
     }
     // minute
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 1},
@@ -557,7 +561,7 @@ TEST_F(FunctionManagerTest, functionCall) {
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 1},
@@ -569,7 +573,7 @@ TEST_F(FunctionManagerTest, functionCall) {
     }
     // second
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 1},
@@ -580,7 +584,7 @@ TEST_F(FunctionManagerTest, functionCall) {
         EXPECT_EQ(res, Value::kNullBadData);
     }
     {
-        auto result = FunctionManager::get("datetime", 1);
+        auto result = FunctionManager::get("datetime", kLiteralTimeParaNumber);
         ASSERT_TRUE(result.ok());
         auto res = std::move(result).value()({Map({{"year", 32767},
                                                    {"month", 1},
