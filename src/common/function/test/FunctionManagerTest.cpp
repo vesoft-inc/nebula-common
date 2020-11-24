33823f6322b33ee959f83a7e9442e70aca813b68
kyle.cao <kyle.cao@vesoft.com>
2020-11-24 14:05:00 +0800

    format

--- src/common/function/test/FunctionManagerTest.cpp
+++ src/common/function/test/FunctionManagerTest.cpp
@@ -44,12 +44,12 @@ std::unordered_map<std::string, std::vector<Value>> FunctionManagerTest::args_ =
     {"one", {-1.2}},
     {"two", {2, 4}},
     {"pow", {2, 3}},
-    {"range1",{1, 5}},
-    {"range2",{1, 5, 2}},
-    {"range3",{5, 1, -2}},
-    {"range4",{1, 5, -2}},
-    {"range5",{5, 1, 2}},
-    {"range6",{1, 5, 0}},
+    {"range1", {1, 5}},
+    {"range2", {1, 5, 2}},
+    {"range3", {5, 1, -2}},
+    {"range4", {1, 5, -2}},
+    {"range5", {5, 1, 2}},
+    {"range6", {1, 5, 0}},
     {"string", {"AbcDeFG"}},
     {"trim", {" abc  "}},
     {"substr", {"abcdefghi", 2, 4}},
@@ -92,12 +92,12 @@ TEST_F(FunctionManagerTest, functionCall) {
         TEST_FUNCTION(log2, args_["int"], 2);
     }
     {
-        TEST_FUNCTION(range,args_["range1"], Value(List({1, 2, 3, 4, 5})));
-        TEST_FUNCTION(range,args_["range2"], Value(List({1, 3, 5})));
-        TEST_FUNCTION(range,args_["range3"], Value(List({5, 3, 1})));
-        TEST_FUNCTION(range,args_["range4"], Value(List(std::vector<Value>{})));
-        TEST_FUNCTION(range,args_["range5"], Value(List(std::vector<Value>{})));
-        TEST_FUNCTION(range,args_["range6"], Value::kNullBadData);
+        TEST_FUNCTION(range, args_["range1"], Value(List({1, 2, 3, 4, 5})));
+        TEST_FUNCTION(range, args_["range2"], Value(List({1, 3, 5})));
+        TEST_FUNCTION(range, args_["range3"], Value(List({5, 3, 1})));
+        TEST_FUNCTION(range, args_["range4"], Value(List(std::vector<Value>{})));
+        TEST_FUNCTION(range, args_["range5"], Value(List(std::vector<Value>{})));
+        TEST_FUNCTION(range, args_["range6"], Value::kNullBadData);
     }
     {
         TEST_FUNCTION(lower, args_["string"], "abcdefg");
@@ -1128,7 +1128,9 @@ TEST_F(FunctionManagerTest, returnType) {
         EXPECT_EQ(Value::Type::LIST, result.value());
     }
     {
-        auto result = FunctionManager::getReturnType("range", {Value::Type::INT, Value::Type::INT, Value::Type::INT});
+        auto result =
+            FunctionManager::getReturnType("range",
+            {Value::Type::INT, Value::Type::INT, Value::Type::INT});
         ASSERT_TRUE(result.ok()) << result.status();
         EXPECT_EQ(Value::Type::LIST, result.value());
     }
