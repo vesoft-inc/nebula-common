1b641a7fedfcb0b5e3c1106fa509de898114a27b
yixinglu <2520865+yixinglu@users.noreply.github.com>
2020-12-30 19:54:34 +0800

    Fix working directory

--- cmake/nebula/GitInfoConfig.cmake
+++ cmake/nebula/GitInfoConfig.cmake
@@ -2,6 +2,7 @@ find_package(Git)
 if (GIT_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
     execute_process(
         COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         OUTPUT_VARIABLE GIT_INFO_SHA
         )
 endif()
