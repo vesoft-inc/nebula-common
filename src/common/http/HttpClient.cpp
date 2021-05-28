44b63d2c10171f26a5f87354bb9f3235c7558f41
tongheihei <47058798+tongheihei@users.noreply.github.com>
2021-05-28 12:03:54 +0800

    libcurl suitable for c++ and delete some code annotation V5

--- src/common/http/HttpClient.cpp
+++ src/common/http/HttpClient.cpp
@@ -19,14 +19,14 @@ struct MemoryStruct {
 };
 
  static size_t WriteMemory(void *contents, size_t size, size_t nmemb, void *userp) {
-  size_t realsize = size * nmemb;
-  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
-  char *ptr = reinterpret_cast<char *> (realloc(mem->memory, mem->size + realsize + 1));
-  mem->memory = ptr;
-  memcpy(&(mem->memory[mem->size]), contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
-  return realsize;
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+    char *ptr = reinterpret_cast<char *> (realloc(mem->memory, mem->size + realsize + 1));
+    mem->memory = ptr;
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+    return realsize;
 }
  StatusOr<std::string> HttpClient::get(const std::string& path, const std::string& /*options*/) {
     CURL *curl;
@@ -54,7 +54,7 @@ struct MemoryStruct {
         }
     }
     free(chunk.memory);
-    return "Libcurl failed";
+    return Status::Error(folly::stringPrintf("Libcurl Failed: %s", path.c_str()));
 }
 
 StatusOr<std::string> HttpClient::post(const std::string& path, const std::string& header) {
@@ -65,33 +65,33 @@ StatusOr<std::string> HttpClient::post(const std::string& path, const std::strin
     int now_total = 0;
     std::string newString = "";
     for ( int i = 0 ; i < len ; i++ ) {
-            if (header[i] == '\"') {
-                now_total++;
-                if (now_total%2 == 1) {
-                    newString = "";
-                    continue;
-                }
-                if (now_total%2 == 0) {
-                    my_curl_list = curl_slist_append(my_curl_list, newString.c_str());
-                }
+        if (header[i] == '\"') {
+            now_total++;
+            if (now_total%2 == 1) {
+                newString = "";
                 continue;
             }
+            if (now_total%2 == 0) {
+                my_curl_list = curl_slist_append(my_curl_list, newString.c_str());
+            }
+            continue;
+        }
             newString+=header[i];
     }
     curl = curl_easy_init();
     if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, path.c_str());
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, my_curl_list);
-    res = curl_easy_perform(curl);
-    curl_slist_free_all(my_curl_list);
-    if (res != CURLE_OK) {
-        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        return "HttpClientHandler failed";
+        curl_easy_setopt(curl, CURLOPT_URL, path.c_str());
+        curl_easy_setopt(curl, CURLOPT_POST, 1L);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, my_curl_list);
+        res = curl_easy_perform(curl);
+        curl_slist_free_all(my_curl_list);
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            return Status::Error(folly::stringPrintf("Http Get Failed: %s", path.c_str()));
+        }
+        curl_easy_cleanup(curl);
     }
-    curl_easy_cleanup(curl);
-  }
-  return "HttpClientHandler successfully";
+    return Status::Error(folly::stringPrintf("Libcurl Failed: %s", path.c_str()));
 }
 
 StatusOr<std::string> HttpClient::post(const std::string& path,
@@ -124,33 +124,33 @@ StatusOr<std::string> HttpClient::put(const std::string& path, const std::string
     int now_total = 0;
     std::string newString = "";
     for ( int i = 0 ; i < len ; i++ ) {
-            if (header[i] == '\"') {
-                now_total++;
-                if (now_total%2 == 1) {
-                    newString = "";
-                    continue;
-                }
-                if (now_total%2 == 0) {
-                    my_curl_list = curl_slist_append(my_curl_list, newString.c_str());
-                }
+        if (header[i] == '\"') {
+            now_total++;
+            if (now_total%2 == 1) {
+                newString = "";
                 continue;
             }
-            newString+=header[i];
+            if (now_total%2 == 0) {
+                my_curl_list = curl_slist_append(my_curl_list, newString.c_str());
+            }
+            continue;
+        }
+        newString+=header[i];
     }
     curl = curl_easy_init();
     if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, path.c_str());
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, my_curl_list);
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "put");
-    res = curl_easy_perform(curl);
-    curl_slist_free_all(my_curl_list);
-    if (res != CURLE_OK) {
-        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        return "HttpClientHandler failed";
+        curl_easy_setopt(curl, CURLOPT_URL, path.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, my_curl_list);
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "put");
+        res = curl_easy_perform(curl);
+        curl_slist_free_all(my_curl_list);
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            return "HttpClientHandler failed";
+        }
+         curl_easy_cleanup(curl);
     }
-    curl_easy_cleanup(curl);
-  }
-  return "HttpClientHandler successfully";
+    return Status::Error(folly::stringPrintf("Libcurl Failed: %s", path.c_str()));
 }
 
 StatusOr<std::string> HttpClient::put(const std::string& path, const folly::dynamic& data) {
@@ -194,7 +194,7 @@ StatusOr<std::string> HttpClient::sendRequest(const std::string& path,
         }
     }
     free(chunk.memory);
-    return "libcurl failed";
+    return Status::Error(folly::stringPrintf("Libcurl Failed: %s", path.c_str()));
 }
 }   // namespace http
 }   // namespace nebula
