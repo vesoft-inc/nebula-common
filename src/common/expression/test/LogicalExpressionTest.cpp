540090fd253b98ffd4a1b4cf139638bad5b353f6
Aiee <18348405+Aiee@users.noreply.github.com>
2021-06-02 20:25:46 +0800

    Add ObjectPool to spaceInfoCache for decoding

--- src/common/expression/test/LogicalExpressionTest.cpp
+++ src/common/expression/test/LogicalExpressionTest.cpp
@@ -163,8 +163,8 @@ TEST_F(LogicalExpressionTest, LogicalCalculation) {
 
         TEST_EXPR(empty OR false AND true AND null XOR empty, Value::kEmpty);
         TEST_EXPR(empty OR false AND true XOR empty OR true, true);
-        // FIXME(aiee)
-        // TEST_EXPR((empty OR false)AND true XOR empty XOR null AND 2 / 0, Value::kNullValue);
+
+        TEST_EXPR((empty OR false) AND true XOR empty XOR null AND 2 / 0, Value::kNullValue);
         // empty OR false AND 2/0
         TEST_EXPR(empty OR false AND true XOR empty XOR null AND 2 / 0, Value::kEmpty);
         TEST_EXPR(empty AND true XOR empty XOR null AND 2 / 0, Value::kNullValue);
